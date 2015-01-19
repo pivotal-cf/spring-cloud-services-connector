af457fafcea910713325502681427eb65db4c0b9
Chris Schaefer <cschaefer@pivotal.io>
2015-01-19 15:27:22 -0800

    Split spring-cloud-pivotal-connector module into core, cloudfoundry, and spring artifacts

---
+++ spring-cloud-pivotal-spring-connector/src/main/java/org/springframework/cloud/pivotal/service/config/ConfigServerServiceConnector.java
@@ -0,0 +1,51 @@
+package org.springframework.cloud.pivotal.service.config;
+
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
+import org.springframework.cloud.service.ServiceInfo;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.cloud.pivotal.service.common.ConfigServerServiceInfo;
+
+import java.util.Collections;
+
+/**
+ * Connector to Config Server service
+ *
+ * @author Chris Schaefer
+ */
+@Configuration
+public class ConfigServerServiceConnector implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, Ordered {
+	private static final String PROPERTY_SOURCE_NAME = "vcapConfigServerUri";
+	private static final String SPRING_CLOUD_CONFIG_URI = "spring.cloud.config.uri";
+
+	private Cloud cloud;
+
+	@Override
+	public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+		if(cloud != null) {
+			return;
+		}
+
+		cloud = new CloudFactory().getCloud();
+
+		for(ServiceInfo serviceInfo : cloud.getServiceInfos()) {
+			if(serviceInfo instanceof ConfigServerServiceInfo) {
+				String uri = ((ConfigServerServiceInfo) serviceInfo).getUri();
+
+				MapPropertySource mapPropertySource = new MapPropertySource(PROPERTY_SOURCE_NAME,
+						Collections.<String, Object>singletonMap(SPRING_CLOUD_CONFIG_URI, uri));
+
+				event.getEnvironment().getPropertySources().addFirst(mapPropertySource);
+			}
+		}
+	}
+
+	@Override
+	public int getOrder() {
+		return Ordered.HIGHEST_PRECEDENCE + 4;
+	}
+}
---
+++ spring-cloud-pivotal-spring-connector/src/main/java/org/springframework/cloud/pivotal/service/gemfire/GemfireXDDataSourceCreator.java
@@ -0,0 +1,19 @@
+package org.springframework.cloud.pivotal.service.gemfire;
+
+import org.springframework.cloud.service.relational.DataSourceCreator;
+import org.springframework.cloud.pivotal.service.common.GemfireXDServiceInfo;
+
+/**
+ * 
+ * @author Ramnivas Laddad
+ *
+ */
+public class GemfireXDDataSourceCreator extends DataSourceCreator<GemfireXDServiceInfo> {
+	
+	private static final String[] DRIVERS = new String[]{"com.pivotal.gemfirexd.internal.jdbc.ClientConnectionPoolDataSource"};
+	private static final String VALIDATION_QUERY = null;
+	
+	public GemfireXDDataSourceCreator() {
+	    super("spring-cloud.gemfirexd.driver", DRIVERS, VALIDATION_QUERY);    
+	}
+}
