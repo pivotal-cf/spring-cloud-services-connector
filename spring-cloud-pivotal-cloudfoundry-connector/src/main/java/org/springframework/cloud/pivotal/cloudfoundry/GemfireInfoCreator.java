76a4d2d0e501fa3ce6aecea6b529adbb20327b48
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2015-02-11 21:47:23 -0500

    Adding cups support

--- spring-cloud-pivotal-cloudfoundry-connector/src/main/java/org/springframework/cloud/pivotal/cloudfoundry/GemfireInfoCreator.java
+++ spring-cloud-pivotal-cloudfoundry-connector/src/main/java/org/springframework/cloud/pivotal/cloudfoundry/GemfireInfoCreator.java
@@ -17,16 +17,32 @@ public class GemfireInfoCreator extends CloudFoundryServiceInfoCreator<GemfireSe
 	public GemfireInfoCreator() {
 		super(new Tags("gemfire"));
 	}
+	
 
 	public GemfireInfoCreator(Tags tags) {
 		super(new Tags("gemfire"));
 	}
 
+	
+	
 	@Override
 	public GemfireServiceInfo createServiceInfo(Map<String, Object> serviceData) {
 		String id = (String) serviceData.get("name");
 		Map<String, Object> credentials = (Map<String, Object>) serviceData.get("credentials");
 		return new GemfireServiceInfo(id, credentials);
+	}
+
+
+	@Override
+	public boolean accept(Map<String, Object> serviceData) {
+		return containsLocators(serviceData) || super.accept(serviceData);
+	}
+	
+	protected boolean containsLocators(Map<String,Object> serviceData){
+		Object locators = getCredentials(serviceData).get("locators");
+		if(locators == null)
+			return false;
+		return true;
 	}
 
 }
