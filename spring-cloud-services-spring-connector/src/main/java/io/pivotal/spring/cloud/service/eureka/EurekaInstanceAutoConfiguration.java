84aaed6ee428c1de4839171562dd7d6c0a80f08f
Chris Schaefer <cschaefer@pivotal.io>
2015-09-04 05:39:11 -0400

    Simplify service registration choices in Spring Cloud Services connectors

---
+++ spring-cloud-services-spring-connector/src/main/java/io/pivotal/spring/cloud/service/eureka/EurekaInstanceAutoConfiguration.java
@@ -0,0 +1,103 @@
+package io.pivotal.spring.cloud.service.eureka;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import java.util.logging.Logger;
+
+/**
+ * Configuration class to configure a Eureka instance's settings based
+ * on route, direct or custom registration methods. Route and direct
+ * use predefined CloudFoundry variables for configuration, custom allows
+ * the use of any eureka.instance.* properties. If no configuration is
+ * present, the default behavior is route.
+ *
+ * @author Chris Schaefer
+ */
+@Configuration
+@ConfigurationProperties("spring.cloud.services")
+public class EurekaInstanceAutoConfiguration {
+	private static Logger LOGGER = Logger.getLogger(EurekaInstanceAutoConfiguration.class.getName());
+
+	private static final int ROUTE_NON_SECURE_PORT = 80;
+	private static final String ROUTE_REGISTRATION_METHOD = "route";
+	private static final String DIRECT_REGISTRATION_METHOD = "direct";
+	private static final String CUSTOM_REGISTRATION_METHOD = "custom";
+
+	@Value("${vcap.application.uris[0]}")
+	private String routeHostName;
+
+	@Value("${cf.instance.ip}")
+	private String directHostName;
+
+	@Value("${cf.instance.port}")
+	private int directPort;
+
+	private String registrationMethod;
+
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean() {
+		if(!StringUtils.isEmpty(registrationMethod)) {
+			LOGGER.info("Eureka registration method: " + registrationMethod);
+
+			if(ROUTE_REGISTRATION_METHOD.equals(registrationMethod)) {
+				return getRouteRegistration();
+			}
+
+			if(DIRECT_REGISTRATION_METHOD.equals(registrationMethod)) {
+				return getDirectRegistration();
+			}
+
+			if(CUSTOM_REGISTRATION_METHOD.equals(registrationMethod)) {
+				return getCustomRegistration();
+			}
+		}
+
+		return getDefaultRegistration();
+	}
+
+	public void setRegistrationMethod(String registrationMethod) {
+		this.registrationMethod = registrationMethod;
+	}
+
+	protected void setRouteHostName(String routeHostName) {
+		this.routeHostName = routeHostName;
+	}
+
+	protected void setDirectHostName(String directHostName) {
+		this.directHostName = directHostName;
+	}
+
+	protected void setDirectPort(int directPort) {
+		this.directPort = directPort;
+	}
+
+	private EurekaInstanceConfigBean getRouteRegistration() {
+		return getEurekaInstanceConfigBean(routeHostName, ROUTE_NON_SECURE_PORT);
+	}
+
+	private EurekaInstanceConfigBean getDirectRegistration() {
+		return getEurekaInstanceConfigBean(directHostName, directPort);
+	}
+
+	private EurekaInstanceConfigBean getCustomRegistration() {
+		return new EurekaInstanceConfigBean();
+	}
+
+	private EurekaInstanceConfigBean getDefaultRegistration() {
+		LOGGER.info("Eureka registration method not provided, defaulting to route");
+		return getRouteRegistration();
+	}
+
+	private EurekaInstanceConfigBean getEurekaInstanceConfigBean(String hostname, int port) {
+		EurekaInstanceConfigBean eurekaInstanceConfigBean = new EurekaInstanceConfigBean();
+		eurekaInstanceConfigBean.setHostname(hostname);
+		eurekaInstanceConfigBean.setNonSecurePort(port);
+
+		return eurekaInstanceConfigBean;
+	}
+}
---
+++ spring-cloud-services-spring-connector/src/test/java/io/pivotal/spring/cloud/service/eureka/EurekaInstanceAutoConfigurationTest.java
@@ -0,0 +1,75 @@
+package io.pivotal.spring.cloud.service.eureka;
+
+import org.junit.Test;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test cases for {@link io.pivotal.spring.cloud.service.eureka.EurekaInstanceAutoConfiguration}
+ *
+ * @author Chris Schaefer
+ */
+public class EurekaInstanceAutoConfigurationTest {
+	private static final int NON_SECURE_PORT = 80;
+	private static final String ROUTE_REGISTRATION_METHOD = "route";
+	private static final String DIRECT_REGISTRATION_METHOD = "direct";
+	private static final String CUSTOM_REGISTRATION_METHOD = "custom";
+	private static final String ROUTE_HOSTNAME = "http://www.route.com";
+	private static final String DIRECT_HOSTNAME = "http://www.direct.com";
+	private static final String DEFAULT_HOSTNAME = ROUTE_HOSTNAME;
+
+	@Test
+	public void testRouteRegistration() {
+		EurekaInstanceAutoConfiguration eurekaInstanceAutoConfiguration = new EurekaInstanceAutoConfiguration();
+		eurekaInstanceAutoConfiguration.setRegistrationMethod(ROUTE_REGISTRATION_METHOD);
+		eurekaInstanceAutoConfiguration.setRouteHostName(ROUTE_HOSTNAME);
+
+		EurekaInstanceConfigBean eurekaInstanceConfigBean = eurekaInstanceAutoConfiguration.eurekaInstanceConfigBean();
+		assertEquals(ROUTE_HOSTNAME, eurekaInstanceConfigBean.getHostname());
+		assertEquals(NON_SECURE_PORT, eurekaInstanceConfigBean.getNonSecurePort());
+	}
+
+	@Test
+	public void testDirectRegistration() {
+		EurekaInstanceAutoConfiguration eurekaInstanceAutoConfiguration = new EurekaInstanceAutoConfiguration();
+		eurekaInstanceAutoConfiguration.setRegistrationMethod(DIRECT_REGISTRATION_METHOD);
+		eurekaInstanceAutoConfiguration.setDirectHostName(DIRECT_HOSTNAME);
+		eurekaInstanceAutoConfiguration.setDirectPort(NON_SECURE_PORT);
+
+		EurekaInstanceConfigBean eurekaInstanceConfigBean = eurekaInstanceAutoConfiguration.eurekaInstanceConfigBean();
+		assertEquals(DIRECT_HOSTNAME, eurekaInstanceConfigBean.getHostname());
+		assertEquals(NON_SECURE_PORT, eurekaInstanceConfigBean.getNonSecurePort());
+	}
+
+	@Test
+	public void testCustomRegistration() {
+		EurekaInstanceAutoConfiguration eurekaInstanceAutoConfiguration = new EurekaInstanceAutoConfiguration();
+		eurekaInstanceAutoConfiguration.setRegistrationMethod(CUSTOM_REGISTRATION_METHOD);
+
+		EurekaInstanceConfigBean eurekaInstanceConfigBean = eurekaInstanceAutoConfiguration.eurekaInstanceConfigBean();
+		assertEquals(getHostName(), eurekaInstanceConfigBean.getHostname());
+		assertEquals(NON_SECURE_PORT, eurekaInstanceConfigBean.getNonSecurePort());
+	}
+
+	@Test
+	public void testDefaultRegistration() {
+		EurekaInstanceAutoConfiguration eurekaInstanceAutoConfiguration = new EurekaInstanceAutoConfiguration();
+		eurekaInstanceAutoConfiguration.setRouteHostName(DEFAULT_HOSTNAME);
+
+		EurekaInstanceConfigBean eurekaInstanceConfigBean = eurekaInstanceAutoConfiguration.eurekaInstanceConfigBean();
+		assertEquals(DEFAULT_HOSTNAME, eurekaInstanceConfigBean.getHostname());
+		assertEquals(NON_SECURE_PORT, eurekaInstanceConfigBean.getNonSecurePort());
+	}
+
+	private String getHostName() {
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException ex) {
+			throw new RuntimeException("Can't get hostname", ex);
+		}
+	}
+}
