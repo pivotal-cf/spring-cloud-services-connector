84728657f5b0fb28af934d5d36c26f5b3dd50301
Craig Walls <cwalls@pivotal.io>
2015-12-14 15:03:28 -0600

    Add bootstrap configuration to set spring.application.name to be the same as vcap.application.name. Can still be overridden in local application configuration.
    [#106348774]
    https://www.pivotaltracker.com/story/show/106348774

---
+++ spring-cloud-services-spring-connector/src/test/java/io/pivotal/spring/cloud/service/config/CloudEnvironmentBootstrapConfigurationIntegrationTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.pivotal.spring.cloud.service.config;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import io.pivotal.spring.cloud.TestApplication;
+
+/**
+ * @author Craig Walls
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestApplication.class)
+@IntegrationTest({"vcap.application.name=app-name"})
+public class CloudEnvironmentBootstrapConfigurationIntegrationTest {
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Test
+	public void environment() throws Exception {
+		Environment env = context.getEnvironment();
+		assertEquals("app-name", env.getProperty("spring.application.name"));
+	}
+
+}
