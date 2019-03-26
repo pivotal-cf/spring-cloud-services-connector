5e12762cb50dd70a7ba27f3775c68cdc2af931f0
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-25 21:32:45 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed But Review Recommended
    These URLs were fixed, but the https status was not OK. However, the https status was the same as the http request or http redirected to an https URL, so they were migrated. Your review is recommended.
    
    * [ ] http://turbine-69a9789a-5df8-49d4-b6d4-81973f076f4e.apps.example.com (UnknownHostException) with 1 occurrences migrated to:
      https://turbine-69a9789a-5df8-49d4-b6d4-81973f076f4e.apps.example.com ([https](https://turbine-69a9789a-5df8-49d4-b6d4-81973f076f4e.apps.example.com) result UnknownHostException).
    * [ ] http://username:password@config-server.mydomain.com (UnknownHostException) with 1 occurrences migrated to:
      https://username:password@config-server.mydomain.com ([https](https://username:password@config-server.mydomain.com) result UnknownHostException).
    * [ ] http://username:password@hystrix-69a9789a-5df8-49d4-b6d4-81973f076f4e.apps.example.com (UnknownHostException) with 1 occurrences migrated to:
      https://username:password@hystrix-69a9789a-5df8-49d4-b6d4-81973f076f4e.apps.example.com ([https](https://username:password@hystrix-69a9789a-5df8-49d4-b6d4-81973f076f4e.apps.example.com) result UnknownHostException).
    * [ ] http://docs.spring.io/spring-boot/docs/current/reference/html/boot- (301) with 1 occurrences migrated to:
      https://docs.spring.io/spring-boot/docs/current/reference/html/boot- ([https](https://docs.spring.io/spring-boot/docs/current/reference/html/boot-) result 404).
    * [ ] http://example.com/token (404) with 2 occurrences migrated to:
      https://example.com/token ([https](https://example.com/token) result 404).
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * [ ] http://cloud.spring.io/spring-cloud-connectors/ with 1 occurrences migrated to:
      https://cloud.spring.io/spring-cloud-connectors/ ([https](https://cloud.spring.io/spring-cloud-connectors/) result 200).
    * [ ] http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html with 1 occurrences migrated to:
      https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html ([https](https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html) result 200).
    * [ ] http://example.com with 2 occurrences migrated to:
      https://example.com ([https](https://example.com) result 200).
    * [ ] http://pivotal.io/platform with 1 occurrences migrated to:
      https://pivotal.io/platform ([https](https://pivotal.io/platform) result 200).
    * [ ] http://docs.pivotal.io/spring-cloud-services/index.html with 1 occurrences migrated to:
      https://docs.pivotal.io/spring-cloud-services/index.html ([https](https://docs.pivotal.io/spring-cloud-services/index.html) result 301).

--- spring-cloud-services-spring-connector/src/main/java/io/pivotal/spring/cloud/config/java/ServiceInfoPropertySourceAdapter.java
+++ spring-cloud-services-spring-connector/src/main/java/io/pivotal/spring/cloud/config/java/ServiceInfoPropertySourceAdapter.java
@@ -17,8 +17,8 @@ import org.springframework.core.env.StandardEnvironment;
  * @author Will Tran
  * 
  * @see <a href=
- *      "http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html#boot-features-external-config">
- *      http://docs.spring.io/spring-boot/docs/current/reference/html/boot-
+ *      "https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html#boot-features-external-config">
+ *      https://docs.spring.io/spring-boot/docs/current/reference/html/boot-
  *      features-external-config.html#boot-features-external-config</a>
  * @param <T>
  *            the {@link ServiceInfo} type
